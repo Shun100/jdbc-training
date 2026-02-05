d0cbe03eb7ed9f5636672b7e891b7d76f6308191
Shun100 <denkidenki55@gmail.com>
2026-02-05 22:45:10 +0900

    WIP

--- jdbc-training/src/main/java/Main.java
+++ jdbc-training/src/main/java/Main.java
@@ -6,17 +6,25 @@ import java.sql.SQLException;
 
 public class Main {
   public static void main(String[] args) {
-    String url = "jdbc:postgresql://host.docker.internal:5432/sampledb?options=-c%20lc_messages=C";
+    String url = "jdbc:postgresql://host.docker.internal:5432/postgres?options=-c%20lc_messages=C";
     String user = "postgres";
     String password = "xxxxxxxx";
 
+    /**
+     * 主なResultSetのデータ取得メソッド
+     * - getString("カラム名"): String
+     * - getInt("カラム名"): int
+     * - getDouble("カラム名"): double
+     * - getDate("カラム名"): java.sql.Date
+     */
+
     try (Connection conn = DriverManager.getConnection(url, user, password);
-          Statement stmt = conn.createStatement();
-          ResultSet rs = stmt.executeQuery("SELECT now()")) {
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM product")) {
             
-            while (rs.next()) {
-              System.out.println("DB time: " + rs.getTimestamp(1));
-            }
+      while (rs.next()) {
+        System.out.println(rs.getString("product_name"));
+      }
     } catch (SQLException e) {
       System.err.println(e);
     }
