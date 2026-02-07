f79878c1246bd15bc1f78d0eb1b98818122e9cb7
Shun100 <denkidenki55@gmail.com>
2026-02-07 19:14:45 +0900

    WIP

--- jdbc-training/src/main/java/Main.java
+++ jdbc-training/src/main/java/Main.java
@@ -22,6 +22,16 @@ public class Main {
      * - getDate("カラム名"): java.sql.Date
      */
 
+    /**
+     * productテーブルの構造
+     * - product_id INTEGER ALWAYS GENERATED AS IDENTITY
+     * - product_name VARCHAR(32) NOT NULL
+     * - product_category VARCHAR(32) NOT NULL
+     * - create_at timestamp DEFAULT CURRENT_TIMESTAMP
+     * - updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+     */
+
+    // SELECT
     try (Connection conn = DriverManager.getConnection(url, user, password);
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT * FROM product")) {
@@ -29,6 +39,43 @@ public class Main {
       while (rs.next()) {
         System.out.println(rs.getString("product_name"));
       }
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
+
+    /**
+     * INSERT
+     * - 構文
+     *  INSERT INTO <テーブル名> (列1, 列2, 列3, ...) VALUES (値1, 値2, 値3, ...);
+     */
+    try (Connection conn = DriverManager.getConnection(url, user, password);
+        Statement stmt = conn.createStatement();) {
+      StringBuilder sqlBuilder = new StringBuilder();
+      sqlBuilder.append("INSERT INTO product ");
+      sqlBuilder.append("(product_name, product_category) ");
+      sqlBuilder.append("VALUES ('詳細LLM', 'AI')");
+
+      int count = stmt.executeUpdate(sqlBuilder.toString()); // INSERTであってもexecuteUpdateメソッドを使う
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
+
+    /**
+     * UPDATE
+     * - 構文
+     *  UPDATE <テーブル名> SET <列名> = <式>, <列名> = <式> ... WHERE <列名> = <式>;
+     */
+    try (Connection conn = DriverManager.getConnection(url, user, password);
+        Statement stmt = conn.createStatement();) {
+      String sql = new StringBuilder()
+        .append("UPDATE ")
+        .append("product ")
+        .append("SET ")
+        .append("product_name = '図解 TCP/IP', ")
+        .append("updated_at = CURRENT_TIMESTAMP(0) ")
+        .append("WHERE product_id = 1")
+        .toString();
+      int count = stmt.executeUpdate(sql);
     } catch (SQLException e) {
       System.err.println(e);
     }
